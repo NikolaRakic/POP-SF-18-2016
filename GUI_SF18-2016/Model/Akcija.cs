7332b22e7eacb47ea97acd90b4b672831774f7ef
Nikola Rakic <nikolarakic15@gmail.com>
2017-11-29 18:07:21 +0100

    Add project files.

---
+++ GUI_SF18-2016/Model/Akcija.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POP_SF_18_2016.Model
+{
+    public class Akcija
+    {
+        public int Id { get; set; }
+        public DateTime DatumPocetka { get; set; }
+        public decimal Popust { get; set; }
+        public DateTime DatumZavrsetka { get; set; }
+        public bool Obrisana { get; set; }
+    }
+}
