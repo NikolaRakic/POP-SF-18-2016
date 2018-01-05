35321fdce7a079c6b28f47b5673f34dcded713c8
NikolaRakic <nikolarakic15@gmail.com>
2018-01-05 09:54:26 +0100

    Add files via upload

--- GUI_SF18-2016/MainWindow.xaml.cs
+++ GUI_SF18-2016/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using POP_SF_18_2016.Model;
+using POP_SF_18_2016.Utils;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,54 @@ namespace GUI_SF18_2016
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string vrstaPrikaza = "namestaj";
+        public static ObservableCollection<Namestaj> listaNamestaja;
         public MainWindow()
         {
             InitializeComponent();
+            dataGrid.Visibility = Visibility.Hidden;
+            glavniMenu.Visibility = Visibility.Hidden;
+            menuPrikazNamestaja.Visibility = Visibility.Hidden;
+            menuPrikazProdaja.Visibility = Visibility.Hidden;
+            menuPrikazKorisnika.Visibility = Visibility.Hidden;
+            btnDodaj.Visibility = Visibility.Hidden;
+
+            listaNamestaja = new ObservableCollection<Namestaj>(GenericSerializer.Deserialize<Namestaj>("namestaj.xml"));
+
+        }
+
+        private void btnPrijaviSe_Click(object sender, RoutedEventArgs e)
+        {
+            lbKorisnickoIme.Visibility = Visibility.Hidden;
+            tbKorisnickoIme.Visibility = Visibility.Hidden;
+            lbLozinka.Visibility = Visibility.Hidden;
+            tbLozinka.Visibility = Visibility.Hidden;
+            btnPrijaviSe.Visibility = Visibility.Hidden;
+
+            dataGrid.Visibility = Visibility.Visible;
+            glavniMenu.Visibility = Visibility.Visible;
+            menuPrikazNamestaja.Visibility = Visibility.Visible;
+            menuPrikazProdaja.Visibility = Visibility.Visible;
+            menuPrikazKorisnika.Visibility = Visibility.Visible;
+            btnDodaj.Visibility = Visibility.Visible;
+
+        }
+
+        private void mniPrikazProdaja(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mniPrikazKorisnika(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mniPrikazNamestaja(object sender, RoutedEventArgs e)
+        {
+            vrstaPrikaza = "namestaj";
+            dataGrid.ItemsSource = listaNamestaja;
+
         }
     }
 }
--- GUI_SF18-2016/Model/Namestaj.cs
+++ GUI_SF18-2016/Model/Namestaj.cs
@@ -1,21 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace POP_SF_18_2016.Model
 {
-    public class Namestaj
+    public class Namestaj: INotifyPropertyChanged
     {
-        public int Id { get; set; }
-        public string Naziv { get; set; }
-        public string Sifra { get; set; }
-        public double Cena { get; set; }
-        public int KolicinaUMagacinu { get; set; }
-        public int AkcijaID { get; set; }
-        public int TipNamestajaID { get; set; }
+        public int id;
+        public string naziv;
+        public string sifra;
+        public double cena;
+        public int kolicinaUMagacinu;
+        public int akcijaID;
+        public int tipNamestajaID;
+        public bool obrisan;
 
-        public bool Obrisan { get; set; }
+        public int Id{ get { return id; } set { if (value != id) { id = value; OnPropertyChanged("Id"); } } }
+        public string Naziv { get { return naziv; } set { if (value != naziv) { naziv = value; OnPropertyChanged("Naziv"); } } }
+        public string Sifra { get { return sifra; } set { if (value != sifra) { sifra = value; OnPropertyChanged("Sifra"); } } }
+        public double Cena { get { return cena; } set { if (value != cena) { cena = value; OnPropertyChanged("Cena"); } } }
+        public int KolicinaUMagacinu { get { return kolicinaUMagacinu; } set { if (value != kolicinaUMagacinu) { kolicinaUMagacinu = value; OnPropertyChanged("KolicinaUMagacinu"); } } }
+        public int AkcijaID { get { return akcijaID; } set { if (value != akcijaID) { akcijaID = value; OnPropertyChanged("AkcijaID"); } } }
+        public int TipNamestajaID { get { return tipNamestajaID; } set { if (value != tipNamestajaID) { tipNamestajaID = value; OnPropertyChanged("TipNamestajaID"); } } }
+        public bool Obrisan { get { return obrisan; } set { if (value != obrisan) { obrisan = value; OnPropertyChanged("Obrisan"); } } }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
     }
 }
